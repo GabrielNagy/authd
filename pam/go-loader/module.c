bcd7d19c56527496901cb1aa3eaaa06ced60d128
Gabriel Nagy <gabriel.nagy@canonical.com>
2024-01-18 13:04:29 +0200

    Update license to LGPL-3.0
    This is less work for us since we license the remainder of the library
    code the same way.

--- pam/go-loader/module.c
+++ pam/go-loader/module.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2023 Marco Trevisan
  *
- * SPDX-License-Identifier: LGPL-2.1-or-later
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
